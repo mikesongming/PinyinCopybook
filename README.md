70798093941079c990753d3dee5658fd2af58b4f
Mike Song <mikesongming@gmail.com>
2023-11-29 13:19:36 +0800

    setuptools

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PinyinCopybook
+
 拼音汉字田字格A4排版后生成png，python纯
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='PinyinCopybook',
+      version=version,
+      description="拼音汉字田字格A4排版后生成png",
+      long_description="""\
+""",
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Text Processing :: Markup :: reStructuredText',
+                   ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords="'pinyin copybook'",
+      author='Mike Song',
+      author_email='',
+      url='',
+      license='Apache 2.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'Pillow>=10.0.1',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
